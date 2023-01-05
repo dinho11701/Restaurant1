43499798d792396de4bda35518035a484c6839cd
Étudiant <oswaldessongue@gmail.com>
2023-01-05 13:04:13 -0500

    page d'accueil faite

---
+++ src/main/java/com/example/restaurant/controller/RestaurantController.java
@@ -0,0 +1,17 @@
+package com.example.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RestaurantController {
+
+    @GetMapping("/")
+    public String home(@RequestParam(defaultValue = "World") String name, ModelMap modelMap){
+        modelMap.put("name",name);
+        return "Pages/home";
+    }
+
+}
