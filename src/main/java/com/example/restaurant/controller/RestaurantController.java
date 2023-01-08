19929c6a81ec50f5400db107f2cd666a2eb70ff5
Étudiant <oswaldessongue@gmail.com>
2023-01-08 01:52:21 -0500

    design des pages pour importer les données d'un restaurant

--- src/main/java/com/example/restaurant/controller/RestaurantController.java
+++ src/main/java/com/example/restaurant/controller/RestaurantController.java
@@ -8,10 +8,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class RestaurantController {
 
-    @GetMapping("/")
+    /*@GetMapping("/")
     public String home(@RequestParam(defaultValue = "World") String name, ModelMap modelMap){
         modelMap.put("name",name);
         return "Pages/home";
+    }*/
+
+    /*@GetMapping("/")
+    public String commencer(){
+        return "Pages/commencer";
+    }*/
+
+    @GetMapping("/")
+    public String deuxiemeEtape(){
+        return "Pages/deuxiemeetape";
     }
+
 
 }
